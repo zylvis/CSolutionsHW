7749691943604267cbbd22d1b5135c5de1355075
zylvis <71163258+zylvis@users.noreply.github.com>
2021-12-02 02:25:41 +0200

    refactoring

--- Homework1/Ternary2s/Converter.cs
+++ Homework1/Ternary2s/Converter.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace Ternary2s
 {
     //RK: add "internal" before "class" method, it is a good practice
     // to explicitly set protection modifiers.
-    class Converter
+    internal class Converter
     {
         public string DecimalToTernary(int n)
         {
             StringBuilder str = new StringBuilder();
-            StringBuilder strTernary = new StringBuilder();
+            //StringBuilder strTernary = new StringBuilder();
 
             while (n != 0)
             {
@@ -20,21 +21,21 @@ namespace Ternary2s
             }
 
             // RK: The transforms below could be replaced with that call
-            // return str.ToString().Reverse().ToString();
+            return str.ToString().Reverse().ToString();
             //
             // Yeah, this is not "fair" as we have not discussed Linq
             // yet, but anyway it is simpler. 
 
 
-            char[] charArray = str.ToString().ToCharArray();
-            Array.Reverse(charArray);
+            //char[] charArray = str.ToString().ToCharArray();
+            //Array.Reverse(charArray);
 
-            for (int i = 0; i < charArray.Length; i++)
-            {
-                strTernary.Append(charArray[i]);
-            }
+            //for (int i = 0; i < charArray.Length; i++)
+            //{
+            //    strTernary.Append(charArray[i]);
+            //}
 
-            return strTernary.ToString();
+            //return strTernary.ToString();
         }
     }
 }

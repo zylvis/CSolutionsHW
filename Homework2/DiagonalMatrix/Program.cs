ec24b7719f337c14cc20de833c939fb0d3b6000d
zylvis <71163258+zylvis@users.noreply.github.com>
2021-12-07 00:23:13 +0200

    Initial commit

---
+++ Homework2/DiagonalMatrix/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiagonalMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

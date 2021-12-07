6dd0a6ffbd1bee4ff5557b2e46141b3379c50ca2
zylvis <71163258+zylvis@users.noreply.github.com>
2021-12-07 05:38:59 +0200

    Adding Track()

--- Homework2/DiagonalMatrix/Matrix.cs
+++ Homework2/DiagonalMatrix/Matrix.cs
@@ -6,13 +6,27 @@ namespace DiagonalMatrix
 {
     public class Matrix
     {
+        private int[] elements;
         private readonly int size;
-              
+                      
         public Matrix(params int[] elements)
         {
+            this.elements = elements;
             size = elements.Length;
         }
+        public int[] Elements => elements;
         public int Size => size;
+
+        public int Track()
+        {
+            int sum = 0;
+            foreach (var item in Elements)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+        
     }
         
 }
--- Homework2/DiagonalMatrix/Program.cs
+++ Homework2/DiagonalMatrix/Program.cs
@@ -6,9 +6,28 @@ namespace DiagonalMatrix
     {
         static void Main(string[] args)
         {
-            Matrix m = new Matrix(1, 2, 3);
-            Console.WriteLine(m.Size);
-            
+            Matrix m = new Matrix(2, 9, 45);
+            Matrix m2 = new Matrix();
+
+            Console.Write($"\"m\" elements:");
+            foreach (var item in m.Elements)
+            {
+                Console.Write($"*{item}");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Size: {m.Size}");
+            Console.WriteLine($"Sum: {m.Track()}");
+
+            Console.Write($"\"m2\" elements:");
+            foreach (var item in m2.Elements)
+            {
+                Console.Write($"*{item}");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Size: {m2.Size}");
+            Console.WriteLine($"Sum: {m2.Track()}");
+
+
         }
     }
 }

30625f8d555403e86d583b7b39a86c1529fc530f
zylvis <71163258+zylvis@users.noreply.github.com>
2021-12-08 23:18:46 +0200

    adding indexein Matrix class

--- Homework2/DiagonalMatrix/Matrix.cs
+++ Homework2/DiagonalMatrix/Matrix.cs
@@ -6,27 +6,46 @@ namespace DiagonalMatrix
 {
     public class Matrix
     {
-        private int[] elements;
-        private readonly int size;
-                      
+        private readonly int[] _diagonalElements;
+        private readonly int _size;
+
         public Matrix(params int[] elements)
         {
-            this.elements = elements;
-            size = elements.Length;
+            _diagonalElements = elements;
+            _size = elements.Length;
         }
-        public int[] Elements => elements;
-        public int Size => size;
+
+
+        public int[] DiagonalElements => _diagonalElements;
+        public int Size => _size;
+
+        public int this[int i, int j]
+        {
+            get
+            {
+                if (i < 0 && j < 0 || i >= Size || j >= Size)
+                {
+                    throw new Exception("Out of range SquareMatrix indexes ");
+                }
+                else if (i == j)
+                {
+                    return DiagonalElements[i];
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
 
         public int Track()
         {
             int sum = 0;
-            foreach (var item in Elements)
+            foreach (var item in DiagonalElements)
             {
                 sum += item;
             }
             return sum;
         }
-        
     }
-        
 }

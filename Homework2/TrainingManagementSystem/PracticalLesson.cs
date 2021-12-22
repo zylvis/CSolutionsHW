aa5183d39e92762eca926844358217fcbb31d5ce
zylvis <71163258+zylvis@users.noreply.github.com>
2021-12-22 04:52:49 +0200

    drop unused string description

--- Homework2/TrainingManagementSystem/PracticalLesson.cs
+++ Homework2/TrainingManagementSystem/PracticalLesson.cs
@@ -8,13 +8,11 @@ namespace TrainingManagementSystem
 {
     internal class PracticalLesson
     {
-        public string DescriptionText { get; set; } 
         public string TaskCondition { get; set; }
         public string Solution { get; set; }
 
-        public PracticalLesson(string descriptionText, string taskCondition, string solution)
+        public PracticalLesson(string taskCondition, string solution)
         {
-            DescriptionText = descriptionText;  
             TaskCondition = taskCondition;
             Solution = solution;
         }

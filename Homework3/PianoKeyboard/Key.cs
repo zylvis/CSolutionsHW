d2a192f9f273ed05981d0938980ce3290678952a
zylvis <71163258+zylvis@users.noreply.github.com>
2021-12-30 22:44:22 +0200

    progress

---
+++ Homework3/PianoKeyboard/Key.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PianoKeyboard;
+
+namespace PianoKeyboard
+{
+    public struct Key
+    {
+        public Key(Note note, string accidental, int octave)
+        {
+            Note = note;
+            Accidental = accidental;
+            Octave = octave;
+
+        }
+
+        public string Note { get; set; }
+        public string Accidental { get; set; }
+        public int Octave { get; set; }
+
+
+        public override string ToString()
+        {
+
+
+            return base.ToString();
+        }
+
+
+    }
+}

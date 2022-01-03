a22975157b9a26de74d9ec824b1bfd3aea2eaea5
zylvis <71163258+zylvis@users.noreply.github.com>
2022-01-03 07:04:33 +0200

    pop

--- Homework3/Stack/Program.cs
+++ Homework3/Stack/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using Stack;
+
 Console.WriteLine("Hello, World!");
+
+
+
+Stack.Stack<string> stack = new();
+
+stack.Push("bybis");
+stack.Push("byraj");
+
+
+
+for (int i = 0; i < 2; i++)
+{
+    Console.WriteLine(stack[i]);
+}
+
+
+
---
+++ Homework3/Stack/Stack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    internal class Stack<T> : IStack<T>
+    {
+        T[] items = new T[0];
+        int size;
+
+        
+        public void Push(T value)
+        {
+            if (size == items.Count())
+            {
+                int newlength = size == 0 ? 1 : size +1;
+                T[] newarray = new T[newlength]; // array with new size   
+                items.CopyTo(newarray, 0);
+                items = newarray;
+            }
+            items[size] = value;
+            size++;
+        }
+
+        public T Pop()
+        {
+            if (size == 0)
+            {
+                throw new InvalidOperationException("Empty");
+            }
+            size--;
+            return items[size];
+        }
+
+        public T this[int index]
+        {
+            get => items[index];
+            set => items[index] = value;
+        }
+
+    }
+}

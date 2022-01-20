9640f5a9ee72e7700120f7b80260a07cdafdf72a
zylvis <71163258+zylvis@users.noreply.github.com>
2022-01-20 08:21:09 +0200

    indexer

---
+++ Homework5/SparseMatrix/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+using SMatrix;
+
+var matrix = new SparseMatrix(100, 100);
+
+matrix.AddItem(0, 0, 1);
+matrix.AddItem(0, 100, 2);
+matrix.AddItem(100, 0, 3);
+matrix.AddItem(100, 100, 4);
+
+foreach (var item in matrix.Elements)
+{
+    Console.WriteLine(item.value);
+}
+
+Console.WriteLine(matrix[2,2]);
---
+++ Homework5/SparseMatrix/SparseMatrix.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMatrix
+{
+    internal class SparseMatrix : IEnumerable<int>
+    {
+        public int RowIndex { get; }
+        public int ColumnIndex { get; }
+
+        public List<(int row, int column, int value)> Elements { get; set; }
+
+
+        public SparseMatrix(int rowIndex, int columnIndex)
+        {
+            this.RowIndex = rowIndex;
+            this.ColumnIndex = columnIndex;
+            Elements = new List<(int, int, int)>();
+        }
+
+        public void AddItem(int row, int column, int item)
+        {
+            Elements.Add((row, column, item));
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int this[int row, int column]
+        {
+            get
+            {
+
+                var tempList = Elements.Where(x => x.row == row && x.column == column).Select(x => x.value).ToList();
+                if (tempList.Count < 1)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return tempList[0];
+                }
+
+
+            }
+            set { }
+        }
+    }
+}

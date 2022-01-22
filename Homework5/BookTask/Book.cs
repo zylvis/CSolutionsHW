a7d32806c44f5e20aa1a2f56cf4ac46c48161af8
zylvis <71163258+zylvis@users.noreply.github.com>
2022-01-22 09:25:12 +0200

    Book

---
+++ Homework5/BookTask/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookTask
+{
+    internal class Book
+    {
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public DateTime? Date { get; set; }
+        public List<string>? Authors { get; set; }
+
+        public Book(string isbn, string title, DateTime date, List<string> authors)
+        {
+            ISBN = isbn;
+            Title = title;
+            Date = date;
+            Authors = authors;
+
+        }
+    }
+}
---
+++ Homework5/BookTask/Catalog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookTask
+{
+    internal class Catalog
+    {
+        public List<Book> Book { get; set; }
+        public Catalog()
+        {
+            Book = new List<Book>();
+        }
+
+        public void AddBook(Book book)
+        {
+            Book.Add(book);
+        }
+
+
+    }
+}

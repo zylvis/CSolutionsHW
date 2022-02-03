39d31e6e0643f669f1871e61f3dabe7411948da2
zylvis <71163258+zylvis@users.noreply.github.com>
2022-02-03 20:01:58 +0200

    Logger class

---
+++ Homework6/Tracking/Logger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracking
+{
+    public class Logger
+    {
+        public string FileName { get; set; }
+        public Logger(string fileName)
+        {
+            FileName = fileName;
+        }
+
+        public void Track(object obj)
+        {
+            
+        }
+
+    }
+}

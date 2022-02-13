ba41507c0b709cb41611eccc8cd6a59e4a24f6fc
zylvis <71163258+zylvis@users.noreply.github.com>
2022-02-13 09:03:26 +0200

    Undo() fixed

--- Homework4/DiagonalMatrixT/MatrixTracker.cs
+++ Homework4/DiagonalMatrixT/MatrixTracker.cs
@@ -9,26 +9,34 @@ namespace DiagonalMatrixT
     public class MatrixTracker<T>
     {
         private int oldIndex;
-        public T? OldValue { get; set; }
-
-        public Matrix<T> Matrix { get; set; }
+        private T? OldValue { get; set; }
+        private MatrixEventArgs<T> eventArgs;
+        private Matrix<T> Matrix { get; set; }
         public MatrixTracker(Matrix<T> matrix)
         {
             Matrix = matrix;
             Matrix.ElementChanged += OnElementChanged;
         }
-        
+
         public void OnElementChanged(object source, MatrixEventArgs<T> args)
         {
-            Console.WriteLine($"Update happened: Index - {args.Index} NewValue - {args.NewValue} + OldValue{args.OldValue}"); // testing
+            Console.WriteLine($"Update happened: Index - {args.Index} NewValue - {args.NewValue} + OldValue - {args.OldValue}"); // testing
             OldValue = args.OldValue;
             oldIndex = args.Index;
+            eventArgs = args;
         }
 
         public Matrix<T> Undo()
         {
-            Matrix[oldIndex, oldIndex] = OldValue;
-            return Matrix;
+            if (eventArgs == null)
+            {
+                return Matrix;
+            }
+            else
+            {
+                Matrix[oldIndex, oldIndex] = OldValue;
+                return Matrix;
+            }
         }
     }
 }

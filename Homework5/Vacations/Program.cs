9e012829794fb3fd87d64fc0c088c02979c06761
zylvis <71163258+zylvis@users.noreply.github.com>
2022-02-17 06:55:59 +0200

    ok

--- Homework5/Vacations/Program.cs
+++ Homework5/Vacations/Program.cs
@@ -5,6 +5,7 @@ var object2 = new Employee("Steve", new DateTime(2021, 3, 6), new DateTime(2021,
 var object3 = new Employee("John", new DateTime(2021, 12, 3), new DateTime(2021, 12, 31));
 var object4 = new Employee("Steve", new DateTime(2021, 4, 3), new DateTime(2021, 9, 3));
 var object5 = new Employee("Jack", new DateTime(2021, 10, 3), new DateTime(2021, 11, 30));
+var object6 = new Employee("Osburn", new DateTime(2021, 12, 3), new DateTime(2021, 11, 30));
 
 Vacation vacation = new Vacation();
 vacation.Add(object1);
@@ -12,6 +13,7 @@ vacation.Add(object2);
 vacation.Add(object3);
 vacation.Add(object4);
 vacation.Add(object5);
+vacation.Add(object6);
 
 Console.WriteLine("Average days of vacations: " + vacation.GetAverageDaysOfVacations());
 
